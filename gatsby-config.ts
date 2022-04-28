431ebfd44e8d868f924c4fc64536ddd1ed9b0067
Matius Hariman Natasampurna <matius@bambu.co>
2022-04-28 13:13:17 +0800

    :sparkles: initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `matiushariman`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
